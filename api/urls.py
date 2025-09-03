bf4f7f88b83d4b45743296afb7444d86c8302d04
thesubodh1 <thesubodh1@gmail.com>
2025-09-03 20:28:58 +0545

    Basic function based view with decorators

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("employees/",views.employees),
+    path("employees/<int:pk>",views.employees_details)
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import JsonResponse
 from employees.models import Employee
+from rest_framework.decorators import api_view
+from .serializers import EmployeeSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+
+
+# create function based views using decorators
+@api_view(["GET","POST"])
 def employees(request):
-   employees = list(Employee.objects.all().values())
-   return JsonResponse(employees,safe=False)
+    if request.method == "GET":
+        employees = Employee.objects.all()
+        serializer = EmployeeSerializer(employees,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    
+    elif request.method == "POST":
+        serializer = EmployeeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        else:
+            return Response (serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        
+
+@api_view(["GET","PUT","DELETE"])
+def employees_details(request,pk):
+    try:
+        employee = Employee.objects.get(pk=pk)
+    except:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+    if request.method == "GET":
+        serializer = EmployeeSerializer(employee)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    
+    elif request.method == "PUT":
+        serializer = EmployeeSerializer(employee,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        
+    elif request.method == "DELETE":
+        employee.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
