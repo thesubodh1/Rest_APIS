ed78d437bb9aa8db9b4700265d4c72441982cb1c
thesubodh1 <thesubodh1@gmail.com>
2025-09-03 19:57:36 +0545

    Database Created and data fetched(Basic)

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    employee_id = models.CharField(max_length=5, unique=True,db_index=True)
+    employee_name = models.CharField(max_length=100)
+    employees_position = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.employee_id}, {self.employee_name}"
