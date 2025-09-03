78bd5ee8859d7f1c1ddb0f248bf2c40834f050e5
thesubodh1 <thesubodh1@gmail.com>
2025-09-03 21:47:50 +0545

    class based views with generic

--- api/views.py
+++ api/views.py
@@ -7,26 +7,12 @@ from rest_framework import mixins,generics
 
 # Create your views here.
 
-# creating class based views with mixins
-class Employees(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPIView):
-    queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializer
-
-    def get(self,request):
-        return self.list(request)
+# creating class based views with generics
+class Employees(generics.ListAPIView,generics.CreateAPIView):
+   queryset = Employee.objects.all()
+   serializer_class = EmployeeSerializer
     
-    def post(self,request):
-        return self.create(request)
-    
-class EmployeesDetails(mixins.RetrieveModelMixin,mixins.UpdateModelMixin,mixins.DestroyModelMixin,generics.GenericAPIView):
-    queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializer
-
-    def get(self,request,pk):
-        return self.retrieve(request,pk)
-    
-    def put(self,request,pk):
-        return self.update(request,pk)
-    
-    def delete(self,request,pk):
-        return self.destroy(request,pk)
+class EmployeesDetails(generics.RetrieveAPIView,generics.UpdateAPIView,generics.DestroyAPIView):
+   queryset = Employee.objects.all()
+   serializer_class = EmployeeSerializer
+   lookup_field = "pk"
