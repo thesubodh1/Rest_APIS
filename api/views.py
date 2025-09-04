cf6d9fd8f5b45e8adfadd3e3e283779a6af2efc8
thesubodh1 <thesubodh1@gmail.com>
2025-09-04 18:21:17 +0545

    custom pagination added

--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import mixins,generics,viewsets
 from employees.serializers import RemarksSerializer,EmployeeSerializer
+from .pagination import CustomPagination
 
 # Create your views here.
 
@@ -11,18 +12,25 @@ from employees.serializers import RemarksSerializer,EmployeeSerializer
 class Employees(generics.ListCreateAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+    pagination_class = CustomPagination
 
 class RemarksView(generics.ListCreateAPIView):
     queryset = Remarks.objects.all()
     serializer_class = RemarksSerializer
+    pagination_class = CustomPagination
+
 
 class EmployeesDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
     lookup_field = "pk"
+    pagination_class = CustomPagination
+
 
 class RemarksDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Remarks.objects.all()
     serializer_class = RemarksSerializer
     lookup_field = "pk"
+    pagination_class = CustomPagination
+
 
--- api_pratice/settings.py
+++ api_pratice/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS' : 'rest_framework.pagination.PageNumberPagination',#LimitOffsetPagination
-    'PAGE_SIZE' : 1
-}
+# REST_FRAMEWORK = {
+#     'DEFAULT_PAGINATION_CLASS' : 'rest_framework.pagination.PageNumberPagination',#LimitOffsetPagination
+#     'PAGE_SIZE' : 1
+# }
