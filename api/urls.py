d686a380d02d07abe26e9539451acfa12e0bafc2
thesubodh1 <thesubodh1@gmail.com>
2025-09-04 10:10:52 +0545

    class based views using viewset with routers added

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('employees',views.Employees,basename='employee')
 
 urlpatterns = [
-    path("employees/",views.Employees.as_view()),
-    path("employees/<int:pk>/",views.EmployeesDetails.as_view())
+    # path("employees/",views.Employees.as_view()),
+    # path("employees/<int:pk>/",views.EmployeesDetails.as_view())
+    path('',include(router.urls))
 ]
