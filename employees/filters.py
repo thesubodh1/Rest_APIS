1029d2a208d138023ddb9b839e0f838b9c2e711d
thesubodh1 <thesubodh1@gmail.com>
2025-09-05 11:32:14 +0545

    filter by id range added

--- employees/filters.py
+++ employees/filters.py
@@ -5,7 +5,16 @@ from .models import Employee
 class EmployeeFilter(django_filters.FilterSet):
     employee_name = django_filters.CharFilter(field_name="employee_name",lookup_expr="icontains")
     employee_position = django_filters.CharFilter(field_name="employees_position",lookup_expr="icontains")
+    id_max = django_filters.CharFilter(method='filter_by_id_range',label="To EMP ID")
+    id_min = django_filters.CharFilter(method='filter_by_id_range',label="From EMP ID")
 
     class Meta:
         model = Employee
-        fields = ['employee_name','employee_position']
+        fields = ['employee_name','employee_position','id_max','id_min']
+
+    def filter_by_id_range(self,queryset,name,value):
+        if name == "id_max":
+            return queryset.filter(employee_id__lte = value)
+        elif name == "id_min":
+            return queryset.filter(employee_id__gte=value)
+        return queryset
